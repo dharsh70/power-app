6e54d0cb167562bcf97d33c5f817b3ea90bb3698
dharsh70 <117129658+dharsh70@users.noreply.github.com>
2024-08-26 09:26:02 +0400

    Create commandsForApp.py

---
+++ commandsForApp.py
@@ -0,0 +1,22 @@
+def getDate(text_widget):
+    text_widget.delete(1.0, tk.END)
+    byteoutput=sp.run(["powershell", "-command", "Get-Date"], capture_output=True)
+    strout=byteoutput.stdout.decode("utf-8")
+    text_widget.insert(tk.END, strout[2:-1])
+
+def getComputerInfo(text_widget):
+    text_widget.delete(1.0, tk.END)
+    byteoutput=sp.run(["powershell", "-command", "Get-ComputerInfo"], capture_output=True)
+    strout=byteoutput.stdout.decode("utf-8")
+    length=len("WindowsRegisteredOwner")
+    strstart=strout.find("WindowsRegisteredOwner")
+    i=length+strstart
+    while strout[i]==" ":
+        i+=1
+    i+=2
+    j=i
+    a=strout.find("\n")
+    while strout[j]!="\n":
+        j+=1
+    text_to_display="System owned by "+strout[i:j]
+    text_widget.insert(tk.END, text_to_display)
